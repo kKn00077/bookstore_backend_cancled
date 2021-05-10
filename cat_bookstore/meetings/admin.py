37c157b31127e279dcc9fe543bfe48745daff31e
Gyuna-Kim <kkn00077@gmail.com>
2021-05-10 16:12:41 +0900

    feat: Add model in admin.py

--- cat_bookstore/meetings/admin.py
+++ cat_bookstore/meetings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Meeting, MeetingAttendApply
 
-# Register your models here.
+
+admin.site.register(Meeting)
+admin.site.register(MeetingAttendApply)
