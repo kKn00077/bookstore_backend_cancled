169fff1351c8e531d4c5e7bed821751949de716a
Gyuna-Kim <kkn00077@gmail.com>
2021-05-10 17:59:52 +0900

    feat: Add accounts.signals.py

--- cat_bookstore/accounts/signals.py
+++ cat_bookstore/accounts/signals.py
@@ -1,13 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from accounts.models import UserAccount
-
-@receiver(post_save, sender=UserAccount)
-def create_user_certification(sender, instance, created, **kwargs):
-    # TODO: 유저 인증 정보 인스턴스 생성 코드 작성
-    pass
+from accounts.models import UserAccount, UserCertification
     
 @receiver(post_save, sender=UserAccount)
 def save_user_certification(sender, instance, **kwargs):
-    # TODO: 유저 인증 정보 인스턴스 save 코드 작성
-    pass
+    certification = UserCertification(account=instance)
+    certification.save()
