f0ff82108dac636bd98baf43a835079c89460e21
Gyuna-Kim <kkn00077@gmail.com>
2021-05-11 15:35:17 +0900

    feat: Setting default drf

--- cat_bookstore/cat_bookstore/settings.py
+++ cat_bookstore/cat_bookstore/settings.py
@@ -74,6 +74,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'cat_bookstore.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ],
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
