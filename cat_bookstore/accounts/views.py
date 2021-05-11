6a7a29d11dddc5a7e323c5876740e5fa461dbec9
Gyuna-Kim <kkn00077@gmail.com>
2021-05-11 16:18:44 +0900

    feat: Temp accounts.view.py code

--- cat_bookstore/accounts/views.py
+++ cat_bookstore/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status, mixins, viewsets, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from .models import UserAccount
 
-# Create your views here.
+
+class AccountViewSet(viewsets.GenericViewSet):
+    queryset = UserAccount.objects.all()
+    permission_classes = [AllowAny]
+
+    @action(methods=['POST'], detail=False)
+    def register(self, request):
+        """
+            회원가입
+        """
+
+        pass
