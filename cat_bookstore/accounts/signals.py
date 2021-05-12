f0f94a92e573d0f551f4bca521dcfe11b5c0c79a
JaewonKwon <springkjw@gmail.com>
2021-05-12 15:37:51 +0900

    feat: 프로젝트 구조 변경

--- cat_bookstore/accounts/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from accounts.models import UserAccount, UserCertification
-    
-@receiver(post_save, sender=UserAccount)
-def save_user_certification(sender, instance, **kwargs):
-    certification = UserCertification(account=instance)
-    certification.save()
