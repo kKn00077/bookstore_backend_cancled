cb4a9e61c6ca50f51553c5ed81ad60812250acd6
Gyuna-Kim <kkn00077@gmail.com>
2021-05-31 17:23:49 +0900

    feat: 서점 카테고리 관련 orm 작성

--- cat_bookstore/apps/profiles/admin.py
+++ cat_bookstore/apps/profiles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, OwnerUserProfile
+from .models import UserProfile, OwnerUserProfile, CategorySubscribe
 
 admin.site.register(UserProfile)
 admin.site.register(OwnerUserProfile)
+admin.site.register(CategorySubscribe)
