f119955fc412d60bfa32f7f2a5aae8a5a265182f
Gyuna-Kim <kkn00077@gmail.com>
2021-05-31 17:33:30 +0900

    fix: 필요없는 import 구문 삭제

--- cat_bookstore/apps/accounts/views.py
+++ cat_bookstore/apps/accounts/views.py
@@ -2,7 +2,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 from .models import UserAccount
 
 from .serializers import (
