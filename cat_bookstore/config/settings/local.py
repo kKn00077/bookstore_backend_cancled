811fcd8e86e63d818091aea6b332cf3388cd3249
Gyuna-Kim <kkn00077@gmail.com>
2021-06-01 11:57:32 +0900

    feat: mysql 관련 설정 및 모듈 추가

--- cat_bookstore/config/settings/local.py
+++ cat_bookstore/config/settings/local.py
@@ -5,9 +5,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', "*"]
 
+# sqlite 환경
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+"""
+
+# mysql 환경
+
+SILENCED_SYSTEM_CHECKS = ['mysql.W003']
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cat_bookstore_local',
+        'USER': 'root',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
--- cat_bookstore/config/settings/prod.py
+++ cat_bookstore/config/settings/prod.py
@@ -1,15 +1,20 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 # TODO: 추후에 변경
-ALLOWED_HOSTS = ['127.0.0.1', "*"]
+ALLOWED_HOSTS = ['Catbookstoredev-env.eba-2kspmfxk.ap-northeast-2.elasticbeanstalk.com', '127.0.0.1', "*"]
 
 # TODO: 추후에 변경
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('RDS_DB_NAME', env('DB_NAME')),
+        'USER': os.environ.get('RDS_USERNAME', env('DB_USERNAME')),
+        'PASSWORD': os.environ.get('RDS_PASSWORD', env('DB_PASSWORD')),
+        'HOST': os.environ.get('RDS_HOSTNAME', env('DB_HOST')),
+        'PORT': os.environ.get('RDS_PORT', env('DB_PORT'))
     }
 }
