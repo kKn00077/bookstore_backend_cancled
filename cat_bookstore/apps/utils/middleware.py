976d8f540a391f219236891a9891e722591c1b7a
Gyuna-Kim <kkn00077@gmail.com>
2021-06-04 16:51:56 +0900

    fix: 최신 버전일 경우 App-Version 헤더 값이 도착하지 않는 버그 수정

--- cat_bookstore/apps/utils/middleware.py
+++ cat_bookstore/apps/utils/middleware.py
@@ -58,28 +58,35 @@ class AppUpdateCheckMiddleware:
         user_agent = request.headers.get('User-Agent', 'undefined').lower()
         app_version=request.headers.get('App-Version', None)
         version_queryset = AppVersion.objects.filter(version=app_version)
-
+        
         if 'mobile' in user_agent:
             # header값이 없거나 버전을 찾을 수 없는 경우
             if version_queryset.count() <= 0:
                 return JsonResponse({"message" : "App-Version is not found in headers"},
                                         status=status.HTTP_400_BAD_REQUEST,
-                                        headers={'App-Version-Status':'UNKNOWN'})
+                                        headers={
+                                            'App-Version-Status':'UNKNOWN', 
+                                            'Is-Mobile' : True})
             
             # 업데이트 필수
             if version_queryset[0].status == VersionStatusChoice.UPDATE_REQUIRED:
                 return JsonResponse({"message" : "App-Version is black-list"},
                                         status=status.HTTP_403_FORBIDDEN,
-                                        headers={'App-Version-Status':VersionStatusChoice.UPDATE_REQUIRED})
+                                        headers={
+                                            'App-Version-Status':VersionStatusChoice.UPDATE_REQUIRED, 
+                                            'Is-Mobile' : True})
 
+                
 
     def process_response(self, request, response):
+        user_agent = request.headers.get('User-Agent', 'undefined').lower()
         status=response.get('App-Version-Status', None)
+        
+        if 'mobile' in user_agent:
+            if status != 'UNKNOWN':
+                app_version=request.headers.get('App-Version')
+                version_queryset = AppVersion.objects.get(version=app_version)
 
-        if status != 'UNKNOWN' and status is not None:
-            app_version=request.headers.get('App-Version')
-            version_queryset = AppVersion.objects.get(version=app_version)
-
-            response['App-Version-Status'] = version_queryset.status
+                response['App-Version-Status'] = version_queryset.status
 
         return response
